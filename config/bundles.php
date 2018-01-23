9b5aebe20313f633cc3249384e81a47436fdcd52
bdurand2 <benjamin.durand.dev@gmail.com>
2018-01-23 20:02:20 +0100

    Init project and install tmdb package for symfony

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Tmdb\SymfonyBundle\TmdbSymfonyBundle::class => ['all' => true],
+];
